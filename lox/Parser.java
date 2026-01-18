c832bc82b248dae27fd7bc5a8a160533a6b6744f
48835606 <danny.ly@student.uq.edu.au>
2026-01-19 00:30:17 +1000

    Added other grammar rules, and finishing primary.

--- lox/Parser.java
+++ lox/Parser.java
@@ -18,19 +18,103 @@ public class Parser {
     }
 
     /**
-     * The equality rule which is expanded from the first rule, expression.
+     * The equality grammar rule which is expanded from the first rule, expression.
      * @return an expression.
      */
     private Expr equality() {
-        Expr expr = comparision();
+        Expr expr = comparison();
 
         while (match(BANG_EQUAL, EQUAL_EQUAL)) {
             Token operator = previous();
-            Expr right = comparision();
+            Expr right = comparison();
             expr = new Expr.Binary(expr, operator, right);
         }
         return expr;
     }
+
+    /**
+     * The comparison grammar rule.
+     * @return an expression.
+     */
+    private Expr comparison() {
+        Expr expr = term();
+
+        while (match(GREATER, GREATER_EQUAL, LESS, LESS_EQUAL)) {
+            Token operator = previous();
+            Expr right = term();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
+    }
+
+    /**
+     * The term grammar rule. (Addition and subtraction).
+     * @return an expression.
+     */
+    private Expr term() {
+        Expr expr = factor();
+
+        while (match(MINUS, PLUS)) {
+            Token operator = previous();
+            Expr right = factor();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
+    }
+
+    /**
+     * The factor grammar rule. (Multiplication and division).
+     * @return an expression.
+     */
+    private Expr factor() {
+        Expr expr = unary();
+
+        while (match(SLASH, STAR)) {
+            Token operator = previous();
+            Expr right = unary();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
+    }
+
+    /**
+     * The unary grammar rule (! or -).
+     * @return an expression.
+     */
+    private Expr unary() {
+        if (match(BANG, MINUS)) {
+            Token operator = previous();
+            Expr right = unary();
+            return new Expr.Unary(operator, right);
+        }
+
+        return primary();
+    }
+
+    private Expr primary() {
+        if (match(FALSE)) return new Expr.Literal(false);
+        if (match(TRUE)) return new Expr.Literal(true);
+        if (match(NIL)) return new Expr.Literal(null);
+
+        if (match(NUMBER, STRING)) {
+            return new Expr.Literal(previous().literal);
+        }
+
+        if (match(LEFT_PAREN)) {
+            Expr expr = expression();
+            consume(RIGHT_PAREN, "Expect ')' after expression.");
+            return new Expr.Grouping(expr);
+        }
+    }
+
+
+
+
+
+
 
     /**
      * Checks to see if the current token has any of the given types.
